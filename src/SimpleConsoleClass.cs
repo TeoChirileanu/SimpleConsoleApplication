5c64594088c464d617d29ce61a74e6a3458298f9
Theodore <teodorchirileanu@gmail.com>
2020-04-18 22:34:31 +0200

    fucking sendgrid

--- src/SimpleConsoleClass.cs
+++ src/SimpleConsoleClass.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.TwiML.Messaging;
@@ -8,18 +14,34 @@ namespace SimpleConsoleProject
 {
     internal static class SimpleConsoleClass
     {
-        private static void Main()
+        private static async Task Main()
         {
             const string message = "Hello World";
             
             DeliverToStandardOutput(message);
-            DeliverToPhoneNumberAsSms(message);
-            // todo: deliver as email
+            // DeliverToPhoneNumberAsSms(message);
+            await DeliverToEmailAddress(message);
+            // todo: extract class, then classes, then folders, then project, then solution
+        }
+
+        private static async Task DeliverToEmailAddress(string message)
+        {
+            var key = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(key);
+            await client.SendEmailAsync(new SendGridMessage
+            {
+                From = new EmailAddress("teodorchirileanu@thecarpathiancoder.dev"),
+                ReplyTo = new EmailAddress("teodorchirileanu@gmail.com"),
+                Subject = "You've got a new message!",
+                PlainTextContent = message
+            });
         }
 
         private static void DeliverToPhoneNumberAsSms(string message)
         {
+            // todo: put these string in a configuration file e.g. config.net or storage.net
             var accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            // todo: update auth token
             var authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
             TwilioClient.Init(accountSid, authToken);
 
